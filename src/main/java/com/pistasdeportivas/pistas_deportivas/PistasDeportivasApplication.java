8788a67d79fc65f862996b96a90cdfd4d9c3a0e7
julu <juanluismatalop@gmail.com>
2024-12-16 20:28:41 +0100

    commit inicial

---
+++ src/main/java/com/pistasdeportivas/pistas_deportivas/PistasDeportivasApplication.java
@@ -0,0 +1,13 @@
+package com.pistasdeportivas.pistas_deportivas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PistasDeportivasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PistasDeportivasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pistasdeportivas/pistas_deportivas/PistasDeportivasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pistasdeportivas.pistas_deportivas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PistasDeportivasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
