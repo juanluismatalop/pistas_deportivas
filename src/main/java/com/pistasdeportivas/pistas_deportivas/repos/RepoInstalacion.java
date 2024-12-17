9d8529c1616cd962ef21eb2e10efaa71cb0b09bd
julu <juanluismatalop@gmail.com>
2024-12-17 11:08:33 +0100

    commit repos Horario e Instalacion

--- src/main/java/com/pistasdeportivas/pistas_deportivas/repos/RepoInstalacion.java
+++ src/main/java/com/pistasdeportivas/pistas_deportivas/repos/RepoInstalacion.java
@@ -1,5 +1,12 @@
 package com.pistasdeportivas.pistas_deportivas.repos;
 
-public class RepoInstalacion {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pistasdeportivas.pistas_deportivas.modelos.Instalacion;
+
+@Repository
+public interface RepoInstalacion extends JpaRepository<Instalacion,Long>{
+
     
 }
