ff55549315beeb188ece68e980100c1eabf490d2
David Rico <davricchi@gmail.com>
2024-12-17 11:07:43 +0100

    controladores y modelos

--- src/main/java/com/pistasdeportivas/pistas_deportivas/modelos/Instalacion.java
+++ src/main/java/com/pistasdeportivas/pistas_deportivas/modelos/Instalacion.java
@@ -1,5 +1,20 @@
 package com.pistasdeportivas.pistas_deportivas.modelos;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Instalacion {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 80)
+    private String nombre;
 }
